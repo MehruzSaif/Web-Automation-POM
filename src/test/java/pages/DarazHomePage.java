32e3cc55b143ae7f3284851de00c51f36500ba0b
Mehruz Saif <mehruz.saif@g.bracu.ac.bd>
2023-08-24 08:07:42 +0600

    daraz home page login button test

--- src/test/java/pages/DarazHomePage.java
+++ src/test/java/pages/DarazHomePage.java
@@ -3,9 +3,13 @@ package pages;
 import org.openqa.selenium.By;
 
 public class DarazHomePage extends BasePage {
+
+    public String DARAZ_HOME_PAGE_URL = "https://www.daraz.com.bd/";
     
     public By SIGNUP_LOGIN_BUTTON = By.xpath("//a[contains(text(),'Signup / Login')]");
     public By CUSTOMER_CARE_BUTTON = By.xpath("//span[contains(text(),'CUSTOMER CARE')]");
     public By HELP_CENTER_BUTTON = By.xpath("//a[(text()='Help Center')]");
+    public By SAVE_MORE_ON_APP = By.xpath("//a[@id='topActionDownload']");
+    public By DARAZ_DONATES = By.xpath("//body/div[@id='J_7459419030']/div[@id='J_LzdSiteNav']/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/a[1]");
 
 }
--- src/test/java/testcases/DarazHomePageTest.java
+++ src/test/java/testcases/DarazHomePageTest.java
@@ -1,5 +1,24 @@
 package testcases;
 
-public class DarazHomePageTest {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import pages.DarazHomePage;
+import utilities.BaseDriverSetup;
+
+public class DarazHomePageTest extends BaseDriverSetup {
+
+    DarazHomePage darazHomePage = new DarazHomePage();
+
+    @Test
+    public void clickSignUpLoginButton() {
+        
+        driver.get(darazHomePage.DARAZ_HOME_PAGE_URL);
+        driver. manage().window().maximize();
+
+        WebElement click_signUp_login = driver.findElement(darazHomePage.SIGNUP_LOGIN_BUTTON);
+        click_signUp_login.click();
+    }
 
 }
