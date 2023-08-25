7aa40383d96e4e422213ed4959baa64d6a09fa62
Mehruz Saif <mehruz.saif@g.bracu.ac.bd>
2023-08-26 02:00:31 +0600

    Daraz reg page class created

--- src/test/java/pages/DarazRegistrationPage.java
+++ src/test/java/pages/DarazRegistrationPage.java
@@ -4,6 +4,9 @@ import org.openqa.selenium.By;
 
 public class DarazRegistrationPage extends BasePage {
 
-    
-    
+    public String DARAZ_REG_PAGE_URL = "https://member.daraz.com.bd/user/register";
+
+    public By Reg_Phone_Field = By.xpath("//input[@placeholder='Please enter your phone number']");
+    public By Verification_Code_Field = By.xpath("//input[@placeholder='Enter your first and last name']");
+
 }
--- src/test/java/testcases/DarazRegPageTest.java
+++ src/test/java/testcases/DarazRegPageTest.java
@@ -1,10 +1,21 @@
 package testcases;
 
+import org.testng.annotations.Test;
+
 import pages.DarazRegistrationPage;
 import utilities.BaseDriverSetup;
 
 public class DarazRegPageTest extends BaseDriverSetup {
 
-    
-    
+    DarazRegistrationPage darazRegPage = new DarazRegistrationPage();
+
+    @Test
+    public void click_signUp_button() throws InterruptedException {
+
+        driver.get(darazRegPage.DARAZ_REG_PAGE_URL);
+        driver. manage().window().maximize();
+        Thread.sleep(3000);
+
+
+    }
 }
